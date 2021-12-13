77f864705c43ddda47d3a79d70ef3a00dcd837f9
Agata Blotnicka <agata.blotnicka@gmail.com>
2021-12-13 12:41:36 +0000

    my changes

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -22,7 +22,7 @@ import java.util.Random;
 @SpringBootApplication
 @RestController
 public class Application {
-  String tactic  = "FFLTLTLTTLTFLTLTTLTLTFLTTFLTTTLTLFTFLTTLFTFLTLFTFLTLTLTLTLTLTLT";
+  String tactic  = "FLTLTLTTLTFLTLTTLTLTFLTTFLTTTLTLFTFLTTLFTFLTTLFTFLTLTTLTTLTLTLTLT";
   int ind = 0;
   static class Self {
     public String href;
